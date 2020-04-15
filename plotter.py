c04097700f623fec6ebfb7160225d2af0702a947
Ovchinnikov <ovgeorge@yandex.ru>
2020-04-15 22:34:11 +0300

    per region selection

--- plotter.py
+++ plotter.py
@@ -12,15 +12,25 @@ def todate(GE):
     d = datetime.datetime(2020, month, day).timetuple().tm_yday
     return d
 
+def todate2(GE):
+    GE = str(GE)
+    day   = int(GE[-2:])
+    month = int(GE[:-2])
+    if month != 3:
+        return -1
+    if day > 27:
+        return -1
+    return day
+
 df = pd.read_csv("comune_giorno.csv", encoding = "iso-8859-1") 
 df = df.replace(9999, 0)
-df['GE'].update( df['GE'].apply(todate) )
-
+df['GE'].update( df['GE'].apply(todate2) )
 import matplotlib.pyplot as plt
 
-index = df['NOME_PROVINCIA'] == 'Bergamo'
+print(df.keys())
+index = df['NOME_PROVINCIA'] == 'Milano'
 df = df[index]
-index = df['GE'] < 88
+index = df['GE'] > - 1
 df88 = df[index].sort_values(by=['GE'])
 df88 = df88.groupby('GE').sum()
 #df.loc[df['column_name'] == some_value]
@@ -30,7 +40,9 @@ plt.plot(list(df88['TOTALE_17'].cumsum()), label = "2017")
 plt.plot(list(df88['TOTALE_18'].cumsum()), label = "2018")
 plt.plot(list(df88['TOTALE_19'].cumsum()), label = "2019")
 plt.plot(list(df88['TOTALE_20'].cumsum()), label = "2020")
-plt.ylabel('Death')
+plt.ylabel('Смерти')
+plt.xlabel('Номер дня с начала месяца -- Март')
+plt.title("Регион: " +  "Милан")
 plt.legend()
 plt.show()
 
