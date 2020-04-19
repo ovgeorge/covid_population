33ecc69e50c0c470d81fa67e17e278efa8483580
ovgeorge <ovgeorge@yandex.ru>
2020-04-20 01:41:48 +0300

    seaborn added

--- plotter.py
+++ plotter.py
@@ -1,9 +1,11 @@
 import pandas as pd 
 import datetime
+import seaborn as sns
 
-#>>> d = datetime.datetime(2020, 3, 28)
-#>>> d.timetuple().tm_yday
-#88
+sns.set()
+sns.set_palette(sns.cubehelix_palette(8))
+
+DAY = 28 #FIXME hardcoded day
 
 def todate(GE):
     GE = str(GE)
@@ -18,7 +20,7 @@ def todate2(GE):
     month = int(GE[:-2])
     if month != 3:
         return -1
-    if day > 27:
+    if day > DAY: 
         return -1
     return day
 
@@ -27,22 +29,32 @@ df = df.replace(9999, 0)
 df['GE'].update( df['GE'].apply(todate2) )
 import matplotlib.pyplot as plt
 
-print(df.keys())
+#index = df['NOME_PROVINCIA'] == 'Bergamo'
+#index = df['NOME_PROVINCIA'] == 'Piacenza'
+#index = df['NOME_PROVINCIA'] == 'Brescia'
 index = df['NOME_PROVINCIA'] == 'Milano'
+
 df = df[index]
 index = df['GE'] > - 1
 df88 = df[index].sort_values(by=['GE'])
 df88 = df88.groupby('GE').sum()
 #df.loc[df['column_name'] == some_value]
-plt.plot(list(df88['TOTALE_15'].cumsum()), label = "2015")
-plt.plot(list(df88['TOTALE_16'].cumsum()), label = "2016")
-plt.plot(list(df88['TOTALE_17'].cumsum()), label = "2017")
-plt.plot(list(df88['TOTALE_18'].cumsum()), label = "2018")
-plt.plot(list(df88['TOTALE_19'].cumsum()), label = "2019")
-plt.plot(list(df88['TOTALE_20'].cumsum()), label = "2020")
-plt.ylabel('Смерти')
-plt.xlabel('Номер дня с начала месяца -- Март')
+x = ["%d/04" % x for x in range(1, 1+DAY)]
+
+plt.plot(x, list(df88['TOTALE_15'].cumsum()),  label = "2015")
+plt.plot(x, list(df88['TOTALE_16'].cumsum()),  label = "2016")
+plt.plot(x, list(df88['TOTALE_17'].cumsum()),  label = "2017")
+plt.plot(x, list(df88['TOTALE_18'].cumsum()),  label = "2018")
+plt.plot(x, list(df88['TOTALE_19'].cumsum()),  label = "2019")
+plt.plot(x, list(df88['TOTALE_20'].cumsum()),  label = "2020")
+plt.xticks(x, rotation='45')
+plt.ylabel('Смерти с нарастающим итогом') 
+plt.xlabel('Дата')
 plt.title("Регион: " +  "Милан")
+
+plt.subplots_adjust(bottom=0.15)
+
 plt.legend()
+
 plt.show()
 
